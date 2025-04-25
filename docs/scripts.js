141b890033f0e0a05b23bc0e899f528e81d8fea4
cosynox <gkos@posteo.de>
2025-04-25 21:06:22 +0200

    more dynamic for the input field

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,9 +1,30 @@
+function write_answer(answer)
+{
+    let antwort = document.querySelector("#antwort");
+    antwort.innerText = answer ;
+}
+
 function greet(event)
 {
     let name = document.querySelector("#name").value;
-    let antwort = document.querySelector("#antwort");
-    antwort.innerText = 'Hello, ' + name ;
+    write_answer("Hello, " + name);
     event.preventDefault();
+}
+
+function keyecho(field)
+{
+    let name = document.querySelector(field);
+    name.addEventListener('keyup', function(event)
+    {
+        if (name.value)
+        {
+            write_answer(`Hello, ${name.value}`);
+        }
+        else
+        {
+            write_answer("Hello, whoever you are");
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded',
@@ -11,5 +32,7 @@ document.addEventListener('DOMContentLoaded',
     {
         let form = document.querySelector("form");
         form.addEventListener("submit", greet);
+        keyecho("#name");
     }
 )
+
